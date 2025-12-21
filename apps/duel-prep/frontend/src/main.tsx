15d6c64d945ea74b5206bd8beb964a19a9605c0a
Ned <hermannned@gmail.com>
2025-12-21 08:50:56 -0500

    feat: api proxy

--- apps/duel-prep/frontend/src/main.tsx
+++ apps/duel-prep/frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
 const queryClient = new QueryClient({
