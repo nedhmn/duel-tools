5a40ba498b3dce7da9c4734e50845389b23208e4
Ned <hermannned@gmail.com>
2026-03-18 09:47:05 -0400

    fix: cubic

--- packages/db/alembic/env.py
+++ packages/db/alembic/env.py
@@ -13,7 +13,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.SYNC_DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.SYNC_DATABASE_URL.replace("%", "%%"))
 target_metadata = Base.metadata
 
 
