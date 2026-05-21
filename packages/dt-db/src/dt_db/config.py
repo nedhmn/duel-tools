6379a7bfae35acebf33d26f59930e918fe66c5cc
Ned <hermannned@gmail.com>
2026-05-21 07:16:54 -0400

    chore: dum

--- packages/dt-db/src/dt_db/config.py
+++ packages/dt-db/src/dt_db/config.py
@@ -10,6 +10,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = Field(
         default="postgresql://duel_tools:duel_tools@localhost:5437/duel_tools"
     )
+    
 
     @cached_property
     def ASYNC_DATABASE_URL(self) -> str:
